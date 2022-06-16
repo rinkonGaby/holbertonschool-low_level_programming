fdd38d0f1cea3ae1c670b28b9a617ee5c79aa22e
rinkonGaby <5235@holbertonschool.com>
2022-06-15 17:32:24 -0700

    alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alfabet
+ * Return: Always 0 (Success)
+ */
+int main ()
+{
+  char c;
+  for (c = 'a' ; c <= 'z' ; c++) putchar (c);
+  putchar('\n');
+  return 0;
+}
