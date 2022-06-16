c3a3dc1f931b3bfeff98cead839ef18022d950ab
rinkonGaby <5235@holbertonschool.com>
2022-06-15 17:50:44 -0700

    alphabet3

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	if(c != 'q' && c != 'e' )
+	{
+		putchar (c) ;
+	}
+	putchar('\n');
+	return (0);
+}
