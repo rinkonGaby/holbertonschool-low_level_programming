db944a9030b0bf81a869ba112b48c8e4ce5f8b76
rinkonGaby <5235@holbertonschool.com>
2022-06-15 17:55:55 -0700

    alphabet3

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -10,9 +10,11 @@ int main(void)
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
-	if(c != 'q' && c != 'e' )
 	{
-		putchar (c) ;
+		if(c != 'q' && c != 'e' )
+		{
+			putchar (c) ;
+		}
 	}
 	putchar('\n');
 	return (0);
