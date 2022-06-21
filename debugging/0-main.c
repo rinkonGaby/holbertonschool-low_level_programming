2a6ba0d0774769b2489d439260392e934c807fda
rinkonGaby <5235@holbertonschool.com>
2022-06-20 21:16:23 -0700

    commit

--- debugging/0-main.c
+++ debugging/0-main.c
@@ -1,13 +1,11 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - assigns a random number to int n everytime
  * it executes, and prints it
  * Return: Always 0 (Success)
  */
-int main(int n)
+void positive_or_negative(int n)
 {
 	if (n > 0)
 		printf("%d is positive\n", n);
@@ -15,5 +13,5 @@ int main(int n)
 		printf("%d is zero\n", n);
 	else if (n < 0)
 		printf("%d is negative\n", n);
-	return (0);
+
 }
